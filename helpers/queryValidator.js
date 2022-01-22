acfc9aed503b15a2c6b36bf6e828eccce3ea1cba
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-01-22 16:09:00 -0500

    query validator module

---
+++ helpers/queryValidator.js
@@ -0,0 +1,34 @@
+const queryValidator = (queryStr, res) => {
+
+    let tags, sortBy, direction;
+
+    // "Tags" Param MUST be present
+    queryStr.tags ?
+        tags = queryStr.tags :
+        res.status(400)
+            .send({ "error": "Tags parameter is required" })
+
+
+    // Check for validity of optional params IF ANY
+    if (queryStr.sortBy) {
+
+        (queryStr.sortBy === 'id' || queryStr.sortBy === 'reads' || queryStr.sortBy === 'likes' || queryStr.sortBy === 'popularity') ?
+            sortBy = queryStr.sortBy :
+            res
+                .status(400)
+                .send({ "error": "sortBy parameter is invalid" })
+    }
+
+    if (queryStr.direction) {
+
+        (queryStr.direction === 'desc' || queryStr.direction === 'asc') ?
+            direction = queryStr.direction :
+            res
+                .status(400)
+                .send({ "error": "Direction parameter is invalid" })
+    }
+
+    return { tags, sortBy, direction }
+}
+
+module.exports = { queryValidator }
--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const { apiResponse } = require('../helpers/apiResponse');
+const { queryValidator } = require('../helpers/queryValidator');
 
 router.get('/ping', (req, res) => {
 
     res
         .status(200)
         .send({ "success": true })
+
 });
 
 router.get('/posts', (req, res) => {
     const queryStr = req.query;
 
-    // "Tags" Param MUST be present
-    if (!(queryStr.tags || queryStr.tag)) {
-        res
-            .status(400)
-            .send({ "error": "Tags parameter is required" })
-    }
-
-    // Check for validity of optional params IF ANY
-
-
-    const tags = queryStr.tags.split(',');
-    apiResponse(tags, res)
+    //Validate the Query
+    let validatedParams = queryValidator(queryStr, res);
+    // If all given paramters are Valid, then proceed to fetch and respond
+    let temptags = validatedParams.tags
+    apiResponse(temptags, res);
 
 })
+
 
 module.exports = router;
